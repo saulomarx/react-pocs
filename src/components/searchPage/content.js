506f68a1cb59290eb96c6415fe1c4e24577b5a6f
Saulo Marx <saulo.marx@cobli.co>
2019-11-04 10:34:53 -0300

    initial commit with structura

---
+++ src/components/searchPage/content.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class Content extends React.Component {
+  render() {
+    return <div>Batata</div>;
+  }
+}
---
+++ src/components/searchPage/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import Content from './content';
+
+const { REACT_APP_API_URL } = process.env;
+const Styles = styled.div``;
+
+class Dictionary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      terms: [],
+      selectedTerm: null,
+      selected: null,
+    };
+  }
+
+  async componentWillMount() {
+    const terms = [];
+    console.log(terms);
+    this.setState({ terms });
+  }
+  render() {
+    const { selectedTerm, selected } = this.state;
+    return (
+      <Styles>
+        <Content />
+      </Styles>
+    );
+  }
+}
+
+export default Dictionary;
